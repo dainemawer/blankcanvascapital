4f8c970983980750d09e3c25a57b6a31de9910d0
Daine Mawer <hello@dainemawer.com>
2023-08-28 17:20:51 +0200

    added contact form, fixed image blurry

--- src/lib/submit.ts
+++ src/lib/submit.ts
@@ -12,21 +12,13 @@ export const doSubmit = (
 			}
 
 			const body = {
-				to: 'hello@dainemawer.com',
 				from: data.email,
 				name: data.name,
 				message: data.message,
-				subject: `⚡️ thelazyload | Form submission`,
-				text_body:
-					`Hey Daine, you have a new submission from thelazyload.com.
-							\n\n${data.message}`,
-				html_body:
-					`<p>Hey Daine, you have a new submission from thelazyload.com.</p>
-							\n\n<p>From: ${data.name} - ${data.email}</p>\n\n
-							<p>Message: ${data.message}</p>`,
+				subject: data.subject,
 			};
 
-			fetch('/api/mail', {
+			fetch('/api/send', {
 				method: 'POST',
 				body: JSON.stringify(body),
 				headers: {
