cc3dcfb5a07f94fbd815d483f599823f23c6dcad
Daine Mawer <hello@dainemawer.com>
2024-10-28 18:50:17 +0200

    remove icons, remove emotion types

--- additional.d.ts
+++ additional.d.ts
@@ -1,11 +1,3 @@
 declare interface Window {
 	gtag: function
 }
-
-declare module '*.svg' {
-	import React = require('react');
-
-	export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-	const src: string;
-	export default src;
-}
--- src/lib/gtag/analytics.tsx
+++ src/lib/gtag/analytics.tsx
@@ -5,34 +5,6 @@
  * @returns {JSX.Element}
  */
 
-import Script from "next/script";
-import { GA_TRACKING_ID } from ".";
-
-export const GoogleAnalytics = (): JSX.Element => {
-	return (
-		<>
-			<Script
-				src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-				strategy="afterInteractive"
-			/>
-			<Script
-				dangerouslySetInnerHTML={{
-					__html: `
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){dataLayer.push(arguments);}
-						gtag('js', new Date());
-						gtag('config', '${GA_TRACKING_ID}', {
-						page_path: window.location.pathname,
-						});
-					`,
-				}}
-				id="gtag-init"
-				strategy="afterInteractive"
-			/>
-		</>
-	);
-};
-
 export const sendToAnalytics = (id, name, label, value) => {
 	window.gtag("event", name, {
 		event_category:
